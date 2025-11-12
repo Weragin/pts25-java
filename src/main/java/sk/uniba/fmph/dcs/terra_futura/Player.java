f0cbdb4075879440eb268a9ff62440e7c56d7673
Weragin <miskoruman@gmail.com>
2025-11-12 17:19:34 +0100

    Added GameState and Player - Player needs to be fully understood and checked first as it is not clear which class should implement InterfaceActivateGrid

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Player.java
@@ -0,0 +1,22 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+import java.security.PublicKey;
+import java.util.HashSet;
+
+public final class Player {
+    public ScoringMethod scoringMethod1;
+    public ScoringMethod scoringMethod2;
+    public ActivationPattern activationPattern1;
+    public ActivationPattern activationPattern2;
+    public Grid grid;
+
+    public Player() {
+        this.scoringMethod1 = new ScoringMethod();
+        this.scoringMethod2 = new ScoringMethod();
+        /* TODO: add correct arguments for the activation patterns that this should contain
+        this.activationPattern1 = new ActivationPattern();
+        this.activationPattern2 = new ActivationPattern();
+        */
+        this.grid = new Grid();
+    }
+}

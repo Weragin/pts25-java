88e81a805e5ba2848af2fc8dbe1cf4c4f0585c9d
Dewliak <veres.z.benedek@gmail.com>
2025-11-18 09:41:12 +0100

    Added unittests + added mockito as dependency

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/MoveCard.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/MoveCard.java
@@ -6,10 +6,14 @@ public class MoveCard {
         if (!grid.canPutCard(gridCoordinate)) {
            return false;
         }
-
+        // we get index and pile from CardSource which have built in restrictions
+        // for the index range I don't know if it is needed here to have a try catch block
         // todo: might be good to put it inside a try/catch block
         // the card is free to place
         Card card = pile.getCard(index);
+        if (card == null) {
+            return false; // no card left there
+        }
         grid.putCard(gridCoordinate, card);
         pile.takeCard(index);
         return true;
---
+++ src/test/java/sk/uniba/fmph/dcs/terra_futura/MoveCardTest.java
@@ -0,0 +1,66 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+import org.junit.Test;
+import org.mockito.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+
+public class MoveCardTest {
+
+    @Test
+    public void testShouldMoveWhenCan() {
+        Pile pile = mock(Pile.class);
+        Grid grid = mock(Grid.class);
+        Card card = mock(Card.class);
+        GridPosition pos = mock(GridPosition.class);
+
+        when(grid.canPutCard(pos)).thenReturn(true);
+        when(pile.getCard(1)).thenReturn(card);
+
+        boolean result = MoveCard.moveCard(1, pile, pos, grid);
+
+        assertTrue(result);
+
+        verify(grid).putCard(pos, card);
+        verify(pile).takeCard(1);
+    }
+
+    @Test
+    public void testDontMoveWhenCant() {
+        Pile pile = mock(Pile.class);
+        Grid grid = mock(Grid.class);
+        Card card = mock(Card.class);
+        GridPosition pos = mock(GridPosition.class);
+
+        when(grid.canPutCard(pos)).thenReturn(false);
+        when(pile.getCard(1)).thenReturn(card);
+
+        boolean result = MoveCard.moveCard(1, pile, pos, grid);
+
+        assertFalse(result);
+
+        verify(grid, never()).putCard(pos, card);
+        verify(pile, never()).takeCard(1);
+    }
+
+    @Test
+    public void testCanPlaceNoCardLeft() {
+        Pile pile = mock(Pile.class);
+        Grid grid = mock(Grid.class);
+        GridPosition pos = mock(GridPosition.class);
+
+        when(grid.canPutCard(pos)).thenReturn(true);
+        when(pile.getCard(1)).thenReturn(null);
+
+        boolean result = MoveCard.moveCard(1, pile, pos, grid);
+
+        assertFalse(result);
+
+        verify(grid, never()).putCard(pos, null);
+        verify(pile, never()).takeCard(1);
+    }
+
+
+}
